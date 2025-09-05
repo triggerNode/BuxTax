6e4f9df40a84ea32fe44314f2aaa25b8390d443f
triggernode <triggernode@gmail.com>
2025-09-05 12:30:07 +0100

    chore(ga): remove hardcoded gtag; route listener only pushes dataLayer pageview; deprecate direct gtag helper

--- src/components/GtagRouteListener.tsx
+++ src/components/GtagRouteListener.tsx
@@ -1,13 +1,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { sendPageview } from "@/lib/analytics";
 import { pushDataLayer } from "@/lib/gtm";
 
 export default function GtagRouteListener() {
   const { pathname, search, hash } = useLocation();
 
   useEffect(() => {
-    sendPageview(pathname + search + hash);
     pushDataLayer({ event: "pageview", page_path: pathname + search + hash });
   }, [pathname, search, hash]);
 
